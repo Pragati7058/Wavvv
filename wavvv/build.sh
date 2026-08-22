296932e695af3e9c3b4316feafb21ddeb9af35c3
Pragati7058 <pragatijadhav7058@gmail.com>
2026-08-22 18:08:11 +0530

    Fix Vercel build by generating .env dynamically

--- wavvv/build.sh
+++ wavvv/build.sh
@@ -3,6 +3,14 @@ echo "Installing Flutter..."
 git clone https://github.com/flutter/flutter.git -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
 flutter config --enable-web
+
+echo "Generating .env file..."
+cat << EOF > .env
+API_BASE_URL=$API_BASE_URL
+SOCKET_URL=$SOCKET_URL
+YOUTUBE_API_KEY=$YOUTUBE_API_KEY
+EOF
+
 echo "Building Flutter Web..."
 flutter pub get
 flutter build web --release
