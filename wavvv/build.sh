88a68bd3f50127dd258dc62a87a8536ab6df877e
Pragati7058 <pragatijadhav7058@gmail.com>
2026-08-22 17:37:29 +0530

    Add Vercel build scripts

---
+++ wavvv/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Installing Flutter..."
+git clone https://github.com/flutter/flutter.git -b stable
+export PATH="$PATH:`pwd`/flutter/bin"
+flutter config --enable-web
+echo "Building Flutter Web..."
+flutter pub get
+flutter build web --release
